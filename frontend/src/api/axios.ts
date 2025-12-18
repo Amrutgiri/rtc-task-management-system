44d21e3e29713bbf3bf518b90257cdbd4e1d66ab
Amrutgiri Goswami <apgoswami.info@gmail.com>
2025-12-18 16:57:15 +0530

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3232",
+  baseURL: "https://rtc-task-management-system.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
